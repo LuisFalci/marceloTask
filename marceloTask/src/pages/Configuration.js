62211a142f3380ae049f21132742abad706f4a10
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-05-23 10:59:53 -0300

    update

--- marceloTask/src/pages/Configuration.js
+++ marceloTask/src/pages/Configuration.js
@@ -4,14 +4,59 @@ import CheckBox from 'expo-checkbox';
 import { Ionicons } from '@expo/vector-icons';
 import { ThemeContext } from '../utils/ThemeProvider';
 import { setTheme } from '../utils/themeStorage';
+import actions from "../services/sqlite/User";
 
 export default function Configuration() {
   const { darkModeEnabled, toggleDarkMode } = useContext(ThemeContext);
   const [notificationEnabled, setNotificationEnabled] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   useEffect(() => {
-    setTheme(darkModeEnabled);
-  }, [darkModeEnabled]);
+    getUserName();
+  }, []);
+
+  const getUserName = async () => {
+    try {
+      const user = await actions.getUser();
+      if (user.length > 0) {
+        setUserName(user[0].name);
+        setUserId(user[0].id);
+      }
+    } catch (error) {
+      console.error('Erro ao obter o nome do usuário:', error);
+    }
+  };
+
+  const editUser = () => {
+    if (userName.length >= 3 && userName.length <= 20 && userId !== '') {
+      actions.update(userId, userName)
+        .then((rowsAffected) => {
+          setErrorMessage('');
+          setSuccessMessage('Usuário editado com sucesso!');
+          // Limpar a mensagem de sucesso após 5 segundos
+          setTimeout(() => {
+            setSuccessMessage('');
+          }, 5000);
+        })
+        .catch((error) => {
+          console.error(`Erro ao editar usuário: ${error}`);
+          setErrorMessage('Erro ao editar usuário. Por favor, tente novamente.');
+          setSuccessMessage('');
+        });
+    } else {
+      if (userName.length < 3) {
+        setErrorMessage('O nome deve ter pelo menos 3 caracteres.');
+      } else if (userName.length > 20) {
+        setErrorMessage('O nome deve ter no máximo 20 caracteres.');
+      } else {
+        setErrorMessage('Usuário inválido. Por favor, verifique os campos.');
+      }
+      setSuccessMessage('');
+    }
+  };
 
   return (
     <View style={[styles.container, darkModeEnabled && styles.darkModeContainer]}>
@@ -20,17 +65,31 @@ export default function Configuration() {
       </View>
       <View style={styles.inputContainer}>
         <Text style={[styles.label, darkModeEnabled && styles.darkModeLabel]}>Nome</Text>
-        <TextInput style={[styles.input, darkModeEnabled && styles.darkModeInput]} />
+        <TextInput
+          style={[
+            styles.input,
+            darkModeEnabled && styles.darkModeInput,
+            darkModeEnabled && styles.darkModePlaceholder,
+          ]}
+          value={userName}
+          onChangeText={setUserName}
+          placeholder="Insira seu nome"
+          placeholderTextColor={darkModeEnabled ? '#aaa' : '#777'}
+        />
+      </View>
+      <View style={styles.errorContainer}>
+        {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+        {successMessage ? <Text style={styles.successText}>{successMessage}</Text> : null}
       </View>
       <View style={styles.checkboxContainer}>
-                <CheckBox
-                    disabled={false}
-                    value={notificationEnabled}
-                    onValueChange={(newValue) => setNotificationEnabled(newValue)}
-                    style={styles.checkbox}
-                />
-                <Text style={[styles.checkboxLabel, darkModeEnabled && styles.darkModeCheckboxLabel]}>Ativar Notificação</Text>
-            </View>
+        <CheckBox
+          disabled={false}
+          value={notificationEnabled}
+          onValueChange={(newValue) => setNotificationEnabled(newValue)}
+          style={styles.checkbox}
+        />
+        <Text style={[styles.checkboxLabel, darkModeEnabled && styles.darkModeCheckboxLabel]}>Ativar Notificação</Text>
+      </View>
       <View style={styles.checkboxContainer}>
         <CheckBox
           disabled={false}
@@ -40,7 +99,7 @@ export default function Configuration() {
         />
         <Text style={[styles.checkboxLabel, darkModeEnabled && styles.darkModeCheckboxLabel]}>Ativar Dark Mode</Text>
       </View>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={editUser}>
         <Text style={styles.buttonText}>Salvar</Text>
       </TouchableOpacity>
     </View>
@@ -82,7 +141,22 @@ const styles = StyleSheet.create({
     color: "#000",
   },
   darkModeInput: {
-    color: "#fff",
+    color: "#fff",  
+  },
+  darkModePlaceholder: {
+    color: '#000',
+  },
+  errorContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+  },
+  successText: {
+    color: 'green',
+    fontSize: 16,
   },
   checkboxContainer: {
     flexDirection: "row",
